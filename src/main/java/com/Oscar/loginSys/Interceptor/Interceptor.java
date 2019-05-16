e029ac5639cc5db2fca8c0fc7ce7cffde4473e74
OscarQQ <qyx1118@outlook.com>
2019-05-16 14:07:36 +0800

    Interceptor setup

--- src/main/java/com/Oscar/loginSys/Interceptor/Interceptor.java
+++ src/main/java/com/Oscar/loginSys/Interceptor/Interceptor.java
@@ -11,17 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 public class Interceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        System.out.println("here");
+        System.out.println("pre handle");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
-        System.out.println("here");
+        System.out.println("post handle");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        System.out.println("here");
+        System.out.println("after completion");
     }
 }
---
+++ src/main/java/com/Oscar/loginSys/InterceptorConfig.java
@@ -0,0 +1,19 @@
+package com.Oscar.loginSys;
+
+import com.Oscar.loginSys.Interceptor.Interceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@SpringBootConfiguration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Autowired
+    private Interceptor myInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(myInterceptor).addPathPatterns("/**").excludePathPatterns("/loginSys/all");
+    }
+}
+
